2747e2c594dfa11372cff513a1cd5b630b6b6d29
amanece13 <abhishek.aman874@gmail.com>
2022-03-15 02:33:35 +0530

    Removing Unnecessary Files

--- server/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./aijay-commerce-firebase-adminsdk-z57wu-35e809e005.json");
-
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin
